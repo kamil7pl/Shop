180c2d886f8cad6ec0078af1416cdd6b5c473e99
Kamil Pietrucha <kamil7.pl@gmail.com>
2021-04-23 23:20:09 +0200

    Produkty z paginacją

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
@@ -11,10 +12,6 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
     /**
      * Show the application dashboard.
@@ -23,6 +20,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+		$products = Product::paginate(9);
+        return view('home', compact('products'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,29 @@
 @extends('layouts.app')
 
+@section('title', 'Shop')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="row">
+		@foreach($products as $product)
+			<div class="col-md-4">
+				<figure class="card card-product">
+					<div class="img-wrap">
+						<img src="{{asset('images/2.jpg')}}" />
+					</div>
+					<figcaption class="info-wrap">
+						<h6 class="title">
+							<a href="#">{{$product->name}}</a>
+						</h6>
+						<div class="price-wrap">
+							<span class="price-new">{{$product->price}} zł</span>
+						</div>
+					</figcaption>
+				</figure>
+			</div>
+		@endforeach
+	</div>
+<div class="row">
+	<div class="col-md-12">
+		{{$products->links()}}
+	</div>
 </div>
 @endsection
